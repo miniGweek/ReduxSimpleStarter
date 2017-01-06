af910fffa6d3f6c1a153a89e73340d247c2b4c86
Rahul Sarkar <rahul.sarkar@bluestone.com.au>
2017-01-06 13:05:10 +1100

    using state to capture video response.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyDEdqtkHKZe5gSsY7R_KpTjHgigk26EzDc';
 
-YTSearch({key : API_KEY, term : 'surfboards'}, function(data){
-    console.log(data);
-});
+YTSearch({key : API_KEY, term : 'surfboards'},  videos => {this.setState({ videos : videos })});
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = { videos : [] };
+    };
 
-const App = () => {
-    return (
-        <div>
-        <SearchBar />
-        </div>);
+    render(){
+        return (
+            <div><SearchBar /></div>
+        );
+    }
 }
-
 
 ReactDOM.render(<App/>,document.querySelector('.container'));
