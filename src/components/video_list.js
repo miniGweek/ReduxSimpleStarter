fa7dc73c7845f53be914d5ec6c35f682724d1474
Rahul Sarkar <rahul.sarkar@bluestone.com.au>
2017-01-06 14:49:45 +1100

    VideoListItem set up.

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -6,7 +6,7 @@ import VideoListItem from './video_list_item';
         const videoItems = videos.map((video) =>{return <VideoListItem key = {video.etag} video = {video} />});
 
         return (
-        <ul className="col-md-4 list-group">
+        <ul className="col-md-4  list-group">
             {videoItems}
         </ul>);
  }
