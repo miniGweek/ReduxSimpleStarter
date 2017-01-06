250d2a265b8b8e2a9080a25613500c9908376029
Rahul Sarkar <rahul.sarkar@bluestone.com.au>
2017-01-06 16:49:33 +1100

    throttling search using lodash

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React,{ Component } from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
@@ -7,21 +8,18 @@ import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyDEdqtkHKZe5gSsY7R_KpTjHgigk26EzDc';
 
-
-
 class App extends Component{
     constructor(props){
         super(props);
         this.state = { videos : [] ,
                        selectedVideo : null};
-
-       
     };
 
     render(){
+        const videoSearch = _.debounce((term) =>{ this.videoSearch(term)},300);
         return (
             <div>
-                <SearchBar onSearchTermChanged = {(term) =>{this.videoSearch(term)} }/>
+                <SearchBar onSearchTermChanged = {videoSearch }/>
                 <VideoDetail video = {this.state.selectedVideo} />
                 <VideoList onVideoSelected = { (selectedVideo) => { this.setState({selectedVideo})}}
                             videos = {this.state.videos}/>
