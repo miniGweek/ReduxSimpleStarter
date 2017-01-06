18cda1020685d70d6131c611fcf977faceebc919
Rahul Sarkar <rahul.sarkar@bluestone.com.au>
2017-01-06 14:59:09 +1100

    corrected video detail extension create video detail component refer the component in index.js pass 1st video object *Fail* with error because the youtube api is a search ajax call and 1st render doesn't have any list of videos. next commit to contain its fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React,{ Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
-import VideoList from './components/video_list.js'
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyDEdqtkHKZe5gSsY7R_KpTjHgigk26EzDc';
@@ -21,6 +22,7 @@ class App extends Component{
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video = {this.state.videos[0]} />
                 <VideoList videos = {this.state.videos}/>
             </div>
         );
