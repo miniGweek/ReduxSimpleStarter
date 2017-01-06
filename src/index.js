71229d5b5ffadf0019c43ed20b8f5595588a927b
Rahul Sarkar <rahul.sarkar@bluestone.com.au>
2017-01-06 12:58:00 +1100

    Importing and using youtube-search-api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyDEdqtkHKZe5gSsY7R_KpTjHgigk26EzDc';
+
+YTSearch({key : API_KEY, term : 'surfboards'}, function(data){
+    console.log(data);
+});
 
 const App = () => {
     return (
