8b306fb32a662e4f86618f872d6b413802dd7b8e
Rahul Sarkar <rahul.sarkar@bluestone.com.au>
2017-01-06 13:07:15 +1100

    correction : YTSearch moved inside constructor so that 'this' is now in scope.

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyDEdqtkHKZe5gSsY7R_KpTjHgigk26EzDc';
 
-YTSearch({key : API_KEY, term : 'surfboards'},  videos => {this.setState({ videos : videos })});
+
+
 class App extends Component{
     constructor(props){
         super(props);
         this.state = { videos : [] };
+
+        YTSearch({key : API_KEY, term : 'surfboards'},
+            ( videos ) => {this.setState({ videos : videos })});
     };
 
     render(){
